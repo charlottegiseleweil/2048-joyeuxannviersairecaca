293c22274dfcf955d8efffddb5397e0dceb9b7e0
charlottegiseleweil <weilcharlotte@gmail.com>
2016-05-19 15:17:29 +0200

    perdu message

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -146,7 +146,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "GAGNÉÉÉÉ yeaaaaah. Bien ouéj gros" : "T'as perdu, t'es grave une merde. J'aurais honte de me regarder dans le miroir à ta place. Va te coucher.";
+  var message = won ? "GAGNÉÉÉÉ *****************. yeeeeaaaaaaah ! Bien ouéj gros" : "T'as perdu ! .................... t'es grave une merde. Va te coucher. ";
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
